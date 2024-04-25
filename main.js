ed545a4002f2e62d4bae672194d3d6eb7596c4fc
bilalbuttnazbutt <164246858+bilalbuttnazbutt@users.noreply.github.com>
2024-04-25 19:19:12 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Assignment # 1
+// storing the person name in a variable
+var personName = "Naz";
+//printing a message to the person
+console.log("Hello ".concat(personName, " Would you like to learn some python today?"));
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Assignment # 1
+
+// storing the person name in a variable
+
+let personName:string = "Naz"
+
+//printing a message to the person
+
+console.log(`Hello ${personName} Would you like to learn some python today?`)
